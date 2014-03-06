4f948db1eddd42d996c03755e7ff172e782cf5bb
John Grubb <therealjohnnygrubb@gmail.com>
2014-03-06 18:08:22 -0500

    rev1

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ $('#trigger').on('click', function(e) {
   e.preventDefault();
 });
 
-$('#main-nav').on('click touchstart', 'li a', function() {
-  $('#main-nav').toggleClass('active');
-});
+//$('#main-nav').on('click touchstart', 'li a', function() {
+//  $('#main-nav').toggleClass('active');
+//});
 
 smoothScroll.init();
 
